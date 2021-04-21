977abc0837ec52c223d4c7722e4ac9142d281358
DanteAlonsoHT <dante1818@hotmail.com>
2021-04-21 18:51:19 -0500

    Fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,4 @@
-#Define the fuction to sort an INT Array
+# Define the fuction to sort an INT Array
 def bubble_sort(array = [])
     array.length.times do
       arranged = true
@@ -13,29 +13,29 @@ def bubble_sort(array = [])
     array
   end
   
-  #Proving bubble_sort() function
-  array_trial_int = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    
-  puts "Input Int Array #{array_trial_int}"
-  puts "Output Int Array #{bubble_sort(array_trial_int)}"
+# Proving bubble_sort() function
+array_trial_int = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   
-  #Define the fuction to sort an STR Array
-  def bubble_sort_by(array)
-    (array.length - 1).times do |i|
-      if (yield array[i], array[i+1]).positive?
-          array[i], array[i+1] = array[i+1], array[i]
-          bubble_sort_by(array) {|first, last| first.length - last.length}
-      end
+puts "Input Int Array #{array_trial_int}"
+puts "Output Int Array #{bubble_sort(array_trial_int)}"
+
+# Define the fuction to sort an STR Array
+def bubble_sort_by(array)
+  (array.length - 1).times do |i|
+    if (yield array[i], array[i+1]).positive?
+        array[i], array[i+1] = array[i+1], array[i]
+        bubble_sort_by(array) {|first, last| first.length - last.length}
     end
-    array
   end
-  
-  array_trial_str = %w[hi hello hey]
-  puts "Input Str Array: #{array_trial_str}"
-  
-  #Proving bubble_sort_by() function
-  bubble_sort_by(array_trial_str) do |first, last|
-    first.length - last.length
-  end
-  
-  puts "Output Str Array: #{array_trial_str}"
+  array
+end
+
+array_trial_str = %w[hi hello hey]
+puts "Input Str Array: #{array_trial_str}"
+
+# Proving bubble_sort_by() function
+bubble_sort_by(array_trial_str) do |first, last|
+  first.length - last.length
+end
+
+puts "Output Str Array: #{array_trial_str}"
