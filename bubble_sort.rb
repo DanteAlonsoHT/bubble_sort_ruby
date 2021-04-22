e4ab9110b12ef309279ce8cf5a278de53ce4217d
DanteAlonsoHT <dante1818@hotmail.com>
2021-04-21 22:23:58 -0500

    Remove debbuging instructions - Improve bubble sort loops

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,10 @@
-# Define the fuction to sort an INT Array
 def bubble_sort(array = [])
   array.length.times do
-    arranged = true
-    break unless arranged
-
     (array.length - 1).times do |i|
       left_element = array[i]
       right_element = array[i + 1]
       next unless array[i] > array[i + 1]
 
-      arranged = false
       array[i] = right_element
       array[i + 1] = left_element
     end
@@ -17,13 +12,8 @@ def bubble_sort(array = [])
   array
 end
 
-# Proving bubble_sort() function
 array_trial_int = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-puts "Input Int Array #{array_trial_int}"
-puts "Output Int Array #{bubble_sort(array_trial_int)}"
-
-# Define the fuction to sort an STR Array
 def bubble_sort_by(array)
   (array.length - 1).times do |i|
     if (yield array[i], array[i + 1]).positive?
@@ -35,11 +25,7 @@ def bubble_sort_by(array)
 end
 
 array_trial_str = %w[hi hello hey]
-puts "Input Str Array: #{array_trial_str}"
 
-# Proving bubble_sort_by() function
 bubble_sort_by(array_trial_str) do |first, last|
   first.length - last.length
 end
-
-puts "Output Str Array: #{array_trial_str}"
